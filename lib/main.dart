040caeca4d75a66e645a0353fec421481b688521
Abdelaleem <ahmed.abdaleim@baianat.com>
2024-01-01 09:54:51 +0200

    refactor_home_page

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-       home: TestConstKeyWord(),
+       home: ParentPage(),
           ),
 
 
