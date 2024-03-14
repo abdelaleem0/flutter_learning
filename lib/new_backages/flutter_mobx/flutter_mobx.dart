80bb4cacdfb327792fc65d06ae8b50a75f970c6d
Abdelaleem <ahmed.abdaleim@baianat.com>
2024-03-14 08:57:59 +0200

    feat => mobx package

--- lib/new_backages/flutter_mobx/flutter_mobx.dart
+++ lib/new_backages/flutter_mobx/flutter_mobx.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:learning_flutter/dart_json_converter/dart_to_json.dart';
 import 'package:learning_flutter/new_backages/flutter_mobx/counter_store.dart';
 
 class CounterExample extends StatelessWidget {
